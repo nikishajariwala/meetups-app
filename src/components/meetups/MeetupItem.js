8524377198eb6e5ef64fe2f2c293ab418a364778
Dr. Nikisha Jariwala, Ph.D <106479727+nikishajariwala@users.noreply.github.com>
2022-09-25 17:17:09 +0530

    Add files via upload

---
+++ src/components/meetups/MeetupItem.js
@@ -0,0 +1,44 @@
+import { useContext } from "react";
+import Card from "../UI/Card";
+import classes from "./MeetupItem.module.css";
+import FavoritesContext from "../../store/favorites-context";
+
+function MeetupItem(props) {
+  const favoritesCtx = useContext(FavoritesContext);
+  const itemIsFavorite = favoritesCtx.itemIsFavorite(props.id);
+
+  function toggleFavoritesStatusHandler() {
+    if (itemIsFavorite) {
+      favoritesCtx.removeFavorite(props.id);
+    } else {
+      favoritesCtx.addFavorite({
+        id: props.id,
+        title: props.title,
+        image: props.image,
+        address: props.address,
+        description: props.description,
+      });
+    }
+  }
+  return (
+    <Card>
+      <li className={classes.item}>
+        <div className={classes.image}>
+          <img src={props.image} alt={props.title} />
+        </div>
+        <div classes={classes.content}>
+          <h3>{props.title}</h3>
+          <address>{props.address}</address>
+          <p>{props.description}</p>
+        </div>
+        <div className={classes.actions}>
+          <button onClick={toggleFavoritesStatusHandler}>
+            {itemIsFavorite ? "Remove from Favorites" : "To Favorites"}
+          </button>
+        </div>
+      </li>
+    </Card>
+  );
+}
+
+export default MeetupItem;
